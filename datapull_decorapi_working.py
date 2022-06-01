a0444f7793be20d484639d2a6612399d313f6e06
tirth-clearsense <tpatel@clearsense.com>
2022-06-01 12:43:32 -0400

    update

--- datapull_decorapi_working.py
+++ datapull_decorapi_working.py
@@ -13,7 +13,8 @@ Created on Tue May 17 02:20:57 2022
 import os
 import numpy as np
 import pandas as pd
-from flask import Flask, Response,jsonify
+
+from flask import Flask, jsonify
 from flask import request
 import requests
 
@@ -23,14 +24,10 @@ from base_schema import *
 from db_connection import *
 import json,time
 from pandarallel import pandarallel
-from config import DATA_SYNC_CONFIG
 pandarallel.initialize()
 from producer.send_datastreams_to_azure import datastream_producer
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-
-from config import PERSONICLE_SCHEMA_API
+from config import PERSONICLE_SCHEMA_API,DATA_SYNC_CONFIG
 
 app = Flask(__name__)
 
@@ -38,8 +35,8 @@ def find_personicle_datastream(personicle_data_type):
     stream_information = requests.get(PERSONICLE_SCHEMA_API['MATCH_DICTIONARY_ENDPOINT'], params={
             "data_type": "datastream",
             "stream_name": personicle_data_type
-        })
-    print(stream_information)
+        }, verify=False)
+        
     # personicle_data_description = find_datastream(personicle_data_type)
     if stream_information.status_code != requests.codes.ok:
         logging.warn("Data type {} not present in personicle data dictionary".format(personicle_data_type))
@@ -53,14 +50,11 @@ def validate_personicle_data_packet(data_packet):
     print("Validating data packet: {}".format(data_packet))
     stream_information = requests.post(PERSONICLE_SCHEMA_API['SCHEMA_VALIDATION_ENDPOINT'], params={
             "data_type": "datastream"
-        }, json=data_packet)
+        }, json=data_packet, verify=False)
     print(stream_information.content)
     validation_response = stream_information.json()
     return validation_response['schema_check']
 
-@app.route("/")
-def test_application():
-    return Response("Data cleaning service up")
 
 @app.route('/fetch_data/',methods=['GET'])
 def request_page():
@@ -68,6 +62,7 @@ def request_page():
     user  = request.args.get('user',type=str , default='')
     data_type  = request.args.get('data', type=str ,default='')
     source  = request.args.get('source',type=str , default='')
+    
     sources = source.split(";") if source is not None else None
     #print("sources:",sources)
     ts  = request.args.get('starttime',type=str , default='')
@@ -79,10 +74,15 @@ def request_page():
 
     query_hr='select * from '+ tblname
     
+    
     hr= sqlio.read_sql_query(query_hr,engine)
-    hr['source']=np.where(pd.to_datetime(hr.timestamp)=='2021-12-09','connect','google-fit')
+   
+    # hr['source']=np.where(pd.to_datetime(hr.timestamp)=='2021-12-09','connect','google-fit')
+    
+    
     hr=hr[(hr.individual_id==user)&(hr.source.isin(sources))&(pd.to_datetime(hr.timestamp)>=ts)&(pd.to_datetime(hr.timestamp)<=te)].copy()
-     
+    
+    # print(hr)
     hr['startdate']=hr['timestamp']-pd.Timedelta('1min')
     hr.rename(columns={'timestamp':'enddate','source':'sourcename'},inplace=True)
     hr=hr[['individual_id','startdate','enddate', 'sourcename', 'value', 'unit', 'confidence']].copy()
@@ -94,7 +94,8 @@ def request_page():
     hr['diff_seconds']=hr['diff_seconds']/np.timedelta64(1,'s')
     hr['weight']=np.round(hr['diff_seconds']**(-1),4)
     hr.weight.fillna(1,inplace=True)
-     
+    
+         
     import pandas as pd1
     
     hr['startdate']=hr.parallel_apply(lambda d:pd1.date_range(d['startdate'],d['enddate']-pd1.Timedelta(freq) if d['diff_seconds'] > 0 else d['startdate'],freq=freq),axis=1)
@@ -116,14 +117,18 @@ def request_page():
     hr=pd.melt(hr, id_vars=['individual_id','startdate','enddate','result_stream'], 
                   value_vars=hr.columns[hr.columns.str.contains('weight')]).copy()
     
+    
     hr=hr[hr.value!=0]
     hr['final_source']=hr['variable'].astype(str)+':' +hr['value'].astype(str)
+    
     hr.drop(columns=['variable','value'],inplace=True)
+    
     hr=hr.groupby(['individual_id','startdate','enddate','result_stream'])['final_source'].apply(';'.join).reset_index()
     
     
     print("hr dim:", hr.shape)
     data=hr[['individual_id','startdate','enddate','final_source','result_stream']].copy()
+    
     data.rename(columns={'result_stream':'value','final_source':'Personicle'},inplace=True)
     if personicle_stream_info['ValueType'] == 'Integer':
         data['value'] = data['value'].astype(int)
@@ -171,7 +176,8 @@ def request_page():
         return jsonify({'message': 'Error while sending data packet; {}'.format(e)})
 
 if __name__ == '__main__':
-    # app.run(port=7777, debug=True)
+    #  app.run(port=7777, debug=True)
+
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     print("running server on {}:{}".format(DATA_SYNC_CONFIG['HOST_URL'], DATA_SYNC_CONFIG['HOST_PORT']))
     app.run(DATA_SYNC_CONFIG['HOST_URL'], port=DATA_SYNC_CONFIG['HOST_PORT'], debug=True)#, ssl_context='adho
